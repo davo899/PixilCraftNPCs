9cd82b72320f72a642d54206900d77cd6bcb95f5
David Sadler <djs21@ic.ac.uk>
2023-12-18 16:02:11 +0000

    Changed NPC constructor to use constant NPC type

--- src/main/java/com/selfdot/pixilcraftnpcs/NPCEntity.java
+++ src/main/java/com/selfdot/pixilcraftnpcs/NPCEntity.java
@@ -1,14 +1,13 @@
 package com.selfdot.pixilcraftnpcs;
 
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.World;
 
 public class NPCEntity extends MobEntity {
 
-    protected NPCEntity(EntityType<? extends MobEntity> entityType, World world) {
-        super(entityType, world);
+    protected NPCEntity(World world) {
+        super(PixilCraftNPCs.NPC, world);
     }
 
     @Override
