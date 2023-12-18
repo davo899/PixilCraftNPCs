9bbd1f150216e917b8ce1b21bfa7bf1ce17ac972
David Sadler <djs21@ic.ac.uk>
2023-12-18 16:01:36 +0000

    Made NPCs invulnerable to damage

--- src/main/java/com/selfdot/pixilcraftnpcs/NPCEntity.java
+++ src/main/java/com/selfdot/pixilcraftnpcs/NPCEntity.java
@@ -1,6 +1,7 @@
 package com.selfdot.pixilcraftnpcs;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.World;
 
@@ -8,6 +9,11 @@ public class NPCEntity extends MobEntity {
 
     protected NPCEntity(EntityType<? extends MobEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        return false;
     }
 
 }
